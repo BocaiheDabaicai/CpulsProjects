e9292f3de860b7664e46ae4b67f9b9e99e8b9e6a
Sonder <108516200+BocaiheDabaicai@users.noreply.github.com>
2024-09-24 13:00:48 +0800

    twice change

--- datastruct-papers/2009.cpp
+++ datastruct-papers/2009.cpp
@@ -2,6 +2,15 @@
 // Created by NfryCreator on 2024/9/24.
 //
 
+/*
+ *  （1）答：
+ *  通过使用链表，判断结点位置是否存在，如果存在那么输出结点的值并返回1，否则就返回0.
+ *  （2）答：
+ *  1. 获取该链表的长度（包含头结点）
+ *  2. 用 链表长度 - 结点倒数位值 + 1 得到正序位值
+ *  3. 使用for循环遍历，寻找该结点，如果该结点不存在，则返回0；如果存在，输出该结点的值，并返回1.
+ * */
+
 #include <iostream>
 
 struct ListNode {
@@ -17,10 +26,10 @@ struct ListNode {
 
 class Solution {
 public:
-    int getLength(ListNode *head){
+    int getLength(ListNode *head) {
         ListNode *temp = head;
         int count = 0;
-        while(temp){
+        while (temp) {
             count++;
             temp = temp->next;
         }
@@ -31,8 +40,8 @@ public:
     int NodeGet(ListNode *head, int n) {
         int length = getLength(head);
         ListNode *p = head;
-        for(int i=1;i<length-n+1;i++){
-            if(p->next == nullptr) return 0;
+        for (int i = 1; i < length - n + 1; i++) {
+            if (p->next == nullptr) return 0;
             else p = p->next;
         }
         std::cout << p->val << std::endl;
@@ -42,14 +51,14 @@ public:
 
 int main() {
     ListNode *a = new ListNode(1);
-    ListNode *b = new ListNode(2,a);
-    ListNode *b1 = new ListNode(3,b);
-    ListNode *b2 = new ListNode(4,b1);
-    ListNode *b3 = new ListNode(5,b2);
-    ListNode *head = new ListNode(0,b3);
+    ListNode *b = new ListNode(2, a);
+    ListNode *b1 = new ListNode(3, b);
+    ListNode *b2 = new ListNode(4, b1);
+    ListNode *b3 = new ListNode(5, b2);
+    ListNode *head = new ListNode(0, b3);
 
     Solution *c = new Solution();
-    int d = c->NodeGet(head,8);
+    int d = c->NodeGet(head, 8);
 
     std::cout << d << std::endl;
 }
