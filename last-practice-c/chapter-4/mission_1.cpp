4f3f131561604a63fcec7b78da817337216baee7
Sonder <108516200+BocaiheDabaicai@users.noreply.github.com>
2025-01-06 14:19:24 +0800

    第五章，后续知识书的题目停做

--- last-practice-c/chapter-4/mission_1.cpp
+++ last-practice-c/chapter-4/mission_1.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+// 练习题
+int main_1() {
     /*
      * 4.1
      * 1
@@ -66,6 +67,60 @@ int main() {
      * d:1,3
      * */
 
+
+    return 0;
+}
+
+// 算法题
+int main_2() {
+
+    // 第一、二题
+    /*int a;
+    printf("Enter a three-digit number: ");
+    scanf("%d", &a);
+
+    printf("The reversal is: %d%d%d\n", a % 10, a / 10 % 10, a / 100);*/
+
+    // 第三题
+    /*int a1, a2, a3;
+    printf("Enter a three-digit number: ");
+    scanf("%1d%1d%1d", &a1, &a2, &a3);
+
+    printf("The reversal is: %d%d%d\n", a3, a2, a1);*/
+
+    // 第四题
+    /*int a4;
+    printf("Enter a number between 0 and 32767: ");
+    scanf("%d", &a4);
+
+    printf("In octal, your number is: %d%d%d%d%d\n", a4 / 8 / 8 / 8 / 8 % 8, a4 / 8 / 8 / 8 % 8, a4 / 8 / 8 % 8,
+           a4 / 8 % 8, a4 % 8);*/
+
+    // 第五题
+    /*int d, i1, i2, i3, i4, i5, j1, j2, j3, j4, j5,
+            first_sum, second_sum, total;
+
+    printf("Enter the first 11 digits of a UPC:");
+    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &d, &i1, &i2, &i3, &i4, &i5, &j1, &j2, &j3, &j4, &j5);
+
+    first_sum = d + i2 + i4 + j1 + j3 + j5;
+    second_sum = i1 + i3 + i5 + j2 + j4;
+    total = 3 * first_sum + second_sum;
+
+    printf("Check digit: %d\n", 9 - ((total - 1) % 10));*/
+
+    // 第六题
+    int i1, i2, i3, i4, i5, i6, j1, j2, j3, j4, j5, j6,
+            first_sum, second_sum, total;
+
+    printf("Enter the first 12 digits of a EAN:");
+    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &i1, &i2, &i3, &i4, &i5, &i6, &j1, &j2, &j3, &j4, &j5, &j6);
+
+    first_sum = i2 + i4 + i6 + j2 + j4 + j6;
+    second_sum = i1 + i3 + i5 + j1 + j3 + j5;
+    total = 3 * first_sum + second_sum - 1;
+
+    printf("Check digit: %d\n", 9 - total % 10);
 
     return 0;
 }
---
+++ last-practice-c/chapter-5/mission_1.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main() {
+    /*
+     * 5.1
+     * 1
+     * a:1
+     * b:1
+     * c:1
+     * d:4
+     * 2
+     * a:1?
+     * b:1
+     * c:1
+     * d:1
+     * 3
+     * a:1,3 4 5
+     * b:1,7 8 9
+     * c:1,8 9 9
+     * d:1,2 2 2
+     * 4
+     * if(i<j) i=-1;
+     * else if(i==j) i=0;
+     * else i=1;
+     * */
+
+    /*
+     * 5.2
+     * 5
+     * 打印消息
+     * 6
+     * 不打印
+     * 7
+     * 17,17
+     * 8
+     * if(age >= 13 && age <= 19) teenager = true;
+     * else teenager = false;
+     * 9
+     * 阅读一下
+     * 10
+     * onetwo
+     * 11
+     * switch(area_code){
+     *  case 229:printf("Albany\n");break;
+     *  case 404||470||678||770:printf("Atlanta\n");break;
+     *  case 478:printf("Macon\n");break;
+     *  case 706||762:printf("Columbus\n");break;
+     *  case 912:printf("Savannah\n");break;
+     *  default:printf(“Area code not recognized\n”);
+     * }// 阅读一下答案
+     * */
+
+    return 0;
+}
